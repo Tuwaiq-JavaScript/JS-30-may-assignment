40b0e05a344fa574547a21e023ebb1b00f26ddd8
daad <daad@test.com>
2022-06-07 02:51:42 +0300

    Assignment1

---
+++ easy/ex2.js
@@ -0,0 +1,5 @@
+//2-write a program that takes two numbers and returns the sum of both
+function sum(num1,num2){
+    console.log('The sum of ' + num1 + ' and ' + num2 + ' is= ' + (num1+num2));
+  }
+  sum(4,7);
